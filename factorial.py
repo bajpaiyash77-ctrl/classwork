f545985694db2d99f224af35ae9bf1812faa8e9b
bajpaiyash77-ctrl <bajpaiyash77@gmail.com>
2026-06-29 09:52:51 +0530

    factorial

---
+++ factorial.py
@@ -0,0 +1,12 @@
+'''Factorial of a number '''
+number = int(input("Enter a number to find its factorial: "))
+#--------------------------------------------------------------
+if number < 0:
+    print("Factorial is not defined for negative numbers.")
+elif number == 0:
+    print("The factorial of 0 is 1.")
+else:
+    factorial = 1
+    for i in range(1, number + 1):
+        factorial *= i
+    print(f"The factorial of {number} is: {factorial}")
