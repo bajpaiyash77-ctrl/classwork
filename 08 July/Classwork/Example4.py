ddcf4743984603290f6ea4a7192ad0a182324963
bajpaiyash77-ctrl <bajpaiyash77@gmail.com>
2026-07-08 10:48:08 +0530

    By using Alias

---
+++ 08 July/Classwork/Example4.py
@@ -0,0 +1,10 @@
+import numericcalculation as nc
+
+a=90
+b=10
+
+print("Addition of", a, "and", b, "is:", nc.calculate_sum(a, b))
+print("Subtraction of", a, "and", b, "is:", nc.calculate_difference(a,b))
+print("Multiplication of", a, "and", b, "is:", nc.calculate_multiplication(a, b))
+print("Division of", a, "and", b, "is:", nc.calculate_division(a, b))
+print("Modulus of", a, "and", b, "is:", nc.calculate_modulus(a, b))
