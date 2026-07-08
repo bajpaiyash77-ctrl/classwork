6aee6ed53ea32f56a273b0f6b5f037e2109155f5
bajpaiyash77-ctrl <bajpaiyash77@gmail.com>
2026-07-08 10:38:35 +0530

    Importing Some Specific Content Only

---
+++ 08 July/Classwork/Example3.py
@@ -0,0 +1,7 @@
+from numericcalculation import calculate_sum
+
+a=10
+b=20
+
+print("The sum of", a, "and", b, "is:", calculate_sum(a, b))
+print("The difference of", a, "and", b, "is:", calculate_difference(a, b))
